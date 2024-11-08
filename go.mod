e65168d632e3e45bebfee516b2ef65fb959e1d73
ShvetsovYura <shvetsov.yura@hotmail.com>
2024-11-08 07:38:55 +0300

    Time OTP

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/ShvetsovYura/oy-keeper
+
+go 1.22.2
+
+require github.com/pquerna/otp v1.4.0
+
+require github.com/boombuler/barcode v1.0.1-0.20190219062509-6c824513bacc // indirect
---
+++ internal/services/totp.go
@@ -0,0 +1,69 @@
+package services
+
+import (
+	"bytes"
+	"fmt"
+	"image/png"
+	"os"
+	"time"
+
+	"github.com/pquerna/otp"
+	"github.com/pquerna/otp/totp"
+)
+
+type Totp struct {
+	key *otp.Key
+}
+
+func New(accountName string) *Totp {
+	return &Totp{}
+}
+
+func (t *Totp) Generate(accountName) error {
+	key, err := totp.Generate(totp.GenerateOpts{
+		Issuer:      "OyKeeper",
+		AccountName: accountName,
+	})
+	if err != nil {
+		panic(err)
+	}
+	
+	t.key = key
+	return nil
+}
+
+func (t *Totp) Load(url string) error {
+	key, err:=otp.NewKeyFromURL(url)
+	if err!=nil{
+		return err
+	}
+	t.key = key
+	return nil
+}
+
+func (t *Totp) GenerateImage() {
+	var buf bytes.Buffer
+
+	img, err := t.key.Image(200, 200)
+	if err != nil {
+		panic(err)
+	}
+	png.Encode(&buf, img)
+	os.WriteFile("qr-code.png", buf.Bytes(), 0644)
+}
+
+func (t *Totp) validate(code string) (bool, error) {
+
+	valid, err := totp.Validate(code, t.key.Secret()
+		
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	if valid {
+		println("Valid passcode!")
+		os.Exit(0)
+	} else {
+		println("Invalid passcode!")
+		os.Exit(1)
+	}
+}
